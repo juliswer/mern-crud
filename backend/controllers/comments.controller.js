edb92412d8c9949ae10700254b04446111b088a7
Julian Swerdlin <julianswer36@gmail.com>
2022-03-15 15:16:07 -0300

    uploading base files

--- backend/controllers/comments.controller.js
+++ backend/controllers/comments.controller.js
@@ -3,12 +3,30 @@ const Comment = require("../models/Comment");
 const getComments = async (req, res) => {
   try {
     const comments = await Comment.find();
-    res.json(comments)
+    res.json(comments);
   } catch (error) {
     console.log(error);
   }
 };
 
+const postComment = async (req, res) => {
+  try {
+    const comment = new Comment(req.body);
+    await comment.save();
+    res.json(comment);
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+};
+
+const deleteComment = async (req, res) => {
+  res.json({
+    message: "Delete comment",
+  });
+};
+
 module.exports = {
   getComments,
+  postComment,
+  deleteComment,
 };
--- backend/routes/comments.routes.js
+++ backend/routes/comments.routes.js
@@ -1,8 +1,16 @@
 const { Router } = require("express");
-const { getComments } = require("../controllers/comments.controller");
+const {
+  getComments,
+  postComment,
+  deleteComment,
+} = require("../controllers/comments.controller");
 
 const router = Router();
 
 router.get("/note/:id/comments", getComments);
+
+router.post("/note/:id/comments", postComment);
+
+router.delete("/note/:id/comments/:commentId", deleteComment);
 
 module.exports = router;
