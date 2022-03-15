cfdf9de940293adb5279646bd8d0588286be2fd1
Julian Swerdlin <julianswer36@gmail.com>
2022-03-15 13:59:12 -0300

    uploading base files

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,21 @@
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const notesRoutes = require("./routes/notes.routes");
+const cors = require('cors');
+const {URLDEPLOY} = require('./config') 
 
 const app = express();
 
-module.exports = app
+const corsOptions = {
+    origin: 'http://localhost:3000' || URLDEPLOY,
+}
+
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// Routes
+app.use(notesRoutes);
+
+module.exports = app;
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,6 @@
 const app = require("./app");
-const notesRoutes = require("./routes/notes.routes");
 require("./db");
-const {PORT} = require("./config")
-
-app.use(notesRoutes);
+const { PORT } = require("./config");
 
 app.listen(PORT);
 console.log(`Server is running on port ${PORT}`);
