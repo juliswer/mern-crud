911d9a30c164e16565430cc06c755876696f95d5
Julian Swerdlin <julianswer36@gmail.com>
2022-03-16 12:52:35 -0300

    uploading packagges

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello world</h1>
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 }
