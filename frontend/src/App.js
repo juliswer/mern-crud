3e897f56c9a5641a01c5cc6e8b9d4455771cdad9
Julian Swerdlin <julianswer36@gmail.com>
2022-03-16 13:05:10 -0300

    uploading packagges

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import NoteComment from './pages/NoteComment';
+import NoteDetail from './pages/NoteDetail';
+
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/note/:id" element={<NoteDetail />}></Route>
+      <Route path="/note/:id/comments/:commentId" element={<NoteComment />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/NoteComment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NoteComment = () => {
+  return (
+    <div>NoteComment</div>
+  )
+}
+
+export default NoteComment
