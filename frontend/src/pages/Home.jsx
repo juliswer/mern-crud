6a340a1ede9bf1fa6a8908f83c1d7e1f96bbb71a
Julian Swerdlin <julianswer36@gmail.com>
2022-03-18 20:15:04 -0300

    uploading the home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,30 @@
-import {Typography, Container} from '@mui/material';
+import {
+  Typography,
+  Container,
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+} from "@mui/material";
+import {Link} from 'react-router-dom';
 
 const Home = ({ notes }) => {
   return (
     <Container className="animate__animated animate__fadeInUp">
-      <Typography variant="h2" component="h1">Home</Typography>
+      <Typography variant="h2" component="h1">
+        Home
+      </Typography>
       {notes.map((note) => (
-        <div key={note._id}>
-          <h2>{note.title}</h2>
-        </div>
+        <Card key={note._id} style={{ marginBottom: "30px" }}>
+          <CardContent>
+            <Typography variant="p" component="h6">
+              {note.title}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small"><Link to={`/note/${note._id}`}>See Note</Link></Button>
+          </CardActions>
+        </Card>
       ))}
     </Container>
   );
