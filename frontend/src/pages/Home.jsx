3c15573cbf663b4302b9bf0c754d8b4598de2e8a
Julian Swerdlin <julianswer36@gmail.com>
2022-03-18 20:03:17 -0300

    uploading error page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,15 @@
+import {Typography, Container} from '@mui/material';
+
 const Home = ({ notes }) => {
   return (
-    <div className="animate__animated animate__fadeInUp">
-      <h1>Home</h1>
+    <Container className="animate__animated animate__fadeInUp">
+      <Typography variant="h2" component="h1">Home</Typography>
       {notes.map((note) => (
         <div key={note._id}>
           <h2>{note.title}</h2>
         </div>
       ))}
-    </div>
+    </Container>
   );
 };
 
