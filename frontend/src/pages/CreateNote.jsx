7b5464eb60513acc9319e5dfe7c783424975a87f
Julian Swerdlin <julianswer36@gmail.com>
2022-03-22 19:20:12 -0300

    uploading

--- frontend/src/pages/CreateNote.jsx
+++ frontend/src/pages/CreateNote.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardActions,
@@ -8,9 +8,28 @@ import {
   TextField,
   Button,
 } from "@mui/material";
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
+import NoteAddIcon from "@mui/icons-material/NoteAdd";
+import axios from "axios";
 
 const CreateNote = () => {
+  const [noteFormInfo, setNoteFormInfo] = useState({
+    title: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    setNoteFormInfo({
+      ...noteFormInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const res = await axios.post('http://localhost:4000/api/notes/new', noteFormInfo);
+    console.log(res);
+  };
+
   return (
     <Grid
       container
@@ -18,50 +37,64 @@ const CreateNote = () => {
       className="animate__animated animate__fadeInUp"
     >
       <Grid item xs={6} spacing={3}>
-        <Card
-          style={{
-            marginBottom: "30px",
-            backgroundColor: "rgba(31,41,55,0.7)",
-            color: "#fff",
-            boxShadow: "5px 5px 5px black",
-            padding: "20px",
-          }}
-          variant="outlined"
-        >
-          <CardContent>
-            <Typography
-              variant="h3"
-              component="h1"
-              gutterBottom
-              style={{ textAlign: "center" }}
-            >
-              Create your note!
-            </Typography>
-            <div style={{ display: "flex", justifyContent: "space-around" }}>
-              <TextField
-                label="Write a title"
-                style={{ width: "13vw" }}
-                variant="standard"
-                autoFocus
-                color="warning"
-                InputProps={{ style: { color: "#fff" } }}
-                InputLabelProps={{ style: { color: "#fff" } }}
-              />
-              <TextField
-                label="Write a title"
-                style={{ width: "13vw" }}
-                variant="standard"
-                color="warning"
-                InputProps={{ style: { color: "#fff" } }}
-                InputLabelProps={{ style: { color: "#fff" } }}
-              />
-            </div>
-          </CardContent>
+        <form onSubmit={handleSubmit}>
+          <Card
+            style={{
+              marginBottom: "30px",
+              backgroundColor: "rgba(31,41,55,0.7)",
+              color: "#fff",
+              boxShadow: "5px 5px 5px black",
+              padding: "20px",
+            }}
+            variant="outlined"
+          >
+            <CardContent>
+              <Typography
+                variant="h3"
+                component="h1"
+                gutterBottom
+                style={{ textAlign: "center" }}
+              >
+                Create your note!
+              </Typography>
+              <div style={{ display: "flex", justifyContent: "space-around" }}>
+                <TextField
+                  label="Write a title"
+                  style={{ width: "13vw" }}
+                  variant="standard"
+                  autoFocus
+                  name="title"
+                  color="warning"
+                  InputProps={{ style: { color: "#fff" } }}
+                  InputLabelProps={{ style: { color: "#fff" } }}
+                  onChange={handleChange}
+                />
+                <TextField
+                  label="Write a title"
+                  style={{ width: "13vw" }}
+                  variant="standard"
+                  color="warning"
+                  name="description"
+                  InputProps={{ style: { color: "#fff" } }}
+                  InputLabelProps={{ style: { color: "#fff" } }}
+                  onChange={handleChange}
+                />
+              </div>
+            </CardContent>
 
-          <CardActions>
-            <Button style={{margin: 'auto'}} variant='outlined' color='success' endIcon={<NoteAddIcon />}>Add Note</Button>
-          </CardActions>
-        </Card>
+            <CardActions>
+              <Button
+                style={{ margin: "auto" }}
+                variant="outlined"
+                color="success"
+                endIcon={<NoteAddIcon />}
+                onClick={handleSubmit}
+              >
+                Add Note
+              </Button>
+            </CardActions>
+          </Card>
+        </form>
       </Grid>
     </Grid>
   );
