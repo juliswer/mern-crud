b3cfe6c16f5ece3962bc13dea906c56c6df671b1
Julian Swerdlin <julianswer36@gmail.com>
2022-03-22 19:44:00 -0300

    uploading

--- frontend/src/pages/CreateNote.jsx
+++ frontend/src/pages/CreateNote.jsx
@@ -7,6 +7,7 @@ import {
   Typography,
   TextField,
   Button,
+  Zoom,
 } from "@mui/material";
 import NoteAddIcon from "@mui/icons-material/NoteAdd";
 import axios from "axios";
@@ -52,73 +53,77 @@ const CreateNote = () => {
   };
 
   return (
-    <Grid
-      container
-      style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
-      className="animate__animated animate__fadeInUp"
-    >
-      <Grid item xs={6} spacing={3}>
-        <form onSubmit={handleSubmit}>
-          <Card
-            style={{
-              marginBottom: "30px",
-              backgroundColor: "rgba(31,41,55,0.7)",
-              color: "#fff",
-              boxShadow: "5px 5px 5px black",
-              padding: "20px",
-            }}
-            variant="outlined"
-          >
-            <CardContent>
-              <Typography
-                variant="h3"
-                component="h1"
-                gutterBottom
-                style={{ textAlign: "center" }}
-              >
-                Create your note!
-              </Typography>
-              <div style={{ display: "flex", justifyContent: "space-around" }}>
-                <TextField
-                  label="Write a title"
-                  style={{ width: "13vw" }}
-                  variant="standard"
-                  autoFocus
-                  name="title"
-                  color="warning"
-                  InputProps={{ style: { color: "#fff" } }}
-                  InputLabelProps={{ style: { color: "#fff" } }}
-                  onChange={handleChange}
-                />
-                <TextField
-                  label="Write a title"
-                  style={{ width: "13vw" }}
-                  variant="standard"
-                  color="warning"
-                  name="description"
-                  InputProps={{ style: { color: "#fff" } }}
-                  InputLabelProps={{ style: { color: "#fff" } }}
-                  onChange={handleChange}
-                />
-              </div>
-            </CardContent>
+    <div>
+      <Grid
+        container
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+        className="animate__animated animate__fadeInUp"
+      >
+        <Grid item xs={6} spacing={3}>
+          <form onSubmit={handleSubmit}>
+            <Card
+              style={{
+                marginBottom: "30px",
+                backgroundColor: "rgba(31,41,55,0.7)",
+                color: "#fff",
+                boxShadow: "5px 5px 5px black",
+                padding: "20px",
+              }}
+              variant="outlined"
+            >
+              <CardContent>
+                <Typography
+                  variant="h3"
+                  component="h1"
+                  gutterBottom
+                  style={{ textAlign: "center" }}
+                >
+                  Create your note!
+                </Typography>
+                <div
+                  style={{ display: "flex", justifyContent: "space-around" }}
+                >
+                  <TextField
+                    label="Write a title"
+                    style={{ width: "13vw" }}
+                    variant="standard"
+                    autoFocus
+                    name="title"
+                    color="warning"
+                    InputProps={{ style: { color: "#fff" } }}
+                    InputLabelProps={{ style: { color: "#fff" } }}
+                    onChange={handleChange}
+                  />
+                  <TextField
+                    label="Write a title"
+                    style={{ width: "13vw" }}
+                    variant="standard"
+                    color="warning"
+                    name="description"
+                    InputProps={{ style: { color: "#fff" } }}
+                    InputLabelProps={{ style: { color: "#fff" } }}
+                    onChange={handleChange}
+                  />
+                </div>
+              </CardContent>
 
-            <CardActions>
-              <Button
-                style={{ margin: "auto" }}
-                variant="outlined"
-                color="success"
-                endIcon={<NoteAddIcon />}
-                onClick={handleSubmit}
-              >
-                Add Note
-              </Button>
-            </CardActions>
-          </Card>
-        </form>
-        <ToastContainer />
+              <CardActions>
+                <Button
+                  style={{ margin: "auto" }}
+                  variant="outlined"
+                  color="success"
+                  endIcon={<NoteAddIcon />}
+                  onClick={handleSubmit}
+                >
+                  Add Note
+                </Button>
+              </CardActions>
+            </Card>
+          </form>
+          <ToastContainer />
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 };
 
