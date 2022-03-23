a8d7d3f0b88749b570b45f5afee91aa60cfa5f2e
Julian Swerdlin <julianswer36@gmail.com>
2022-03-23 14:53:04 -0300

    uploading about changes

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Typography, Container} from '@mui/material';
 
 const About = () => {
   return (
-    <div>About</div>
+    <Container className="animate__animated animate__bounceInUp">
+      <Typography variant="h3" component="h1">About Page</Typography>
+    </Container>
   )
 }
 
