600a6766c0e7f2d4ea6f085d30b5c94844deb971
Julian Swerdlin <julianswer36@gmail.com>
2022-03-23 15:25:17 -0300

    uploading about changes

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import {Typography, Container} from '@mui/material';
+import React, {useState} from 'react'
+import {Typography, Container, Button} from '@mui/material';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 const About = () => {
+
+  const [copy, setCopy] = useState(false);
+
   return (
     <Container className="animate__animated animate__bounceInUp">
       <Typography variant="h3" component="h1">About Page</Typography>
+      <Typography variant="h5" component="h2">This app was developed by Julian Swerdlin in March 2022.</Typography>
+      <Typography variant="h6" component="h3">I used the MERN Stack to develop it. If you think you can make something better, you can clone or fork the Github's Repo and post it!</Typography>
+      <Typography variant="h6" component="h3">If you want to know more about me, you can visit my <a href="https://www.linkedin.com/in/julian-swerdlin/">LinkedIn</a>.</Typography>      
+      <CopyToClipboard onCopy={() => setCopy(true)} text="git clone https://github.com/juliswer/mern-crud.git">
+        <Button variant="outlined" color="warning" endIcon={<ContentCopyIcon />}>{copy ? `It's in your clipboard!` : 'Copy'}</Button>
+      </CopyToClipboard>
     </Container>
   )
 }
