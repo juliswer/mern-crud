aca1f92b5c1bd23c256ddde8d5d2f767965f8223
Julián Swerdlin <julianswer36@gmail.com>
2022-03-24 22:21:43 +0100

    Delete backend directory

--- backend/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv/config');
-
-module.exports = {
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/mern-crud-notes-app',
-    PORT: process.env.PORT || 4000,
-    URLDEPLOY: 'https://www.mern-crud.vercel.app'
-}
