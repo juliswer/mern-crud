62a6b597ece94e87851223fde46e81027f826f1f
Julian Swerdlin <julianswer36@gmail.com>
2022-03-24 18:52:04 -0300

    uploading config changes

--- backend/config.js
+++ backend/config.js
@@ -3,5 +3,5 @@ require('dotenv/config');
 module.exports = {
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/mern-crud-notes-app',
     PORT: process.env.PORT || 4000,
-    URLDEPLOY: 'https://www.mern-crud.vercel.app'
+    URLDEPLOY: process.env.URL_DEPLOY
 }
