4872e4c629dfc78e3c73b7c9272c8d963d17a684
Julian Swerdlin <julianswer36@gmail.com>
2022-03-23 22:07:54 -0300

    about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
-import {Typography, Container, Button} from '@mui/material';
+import {Typography, Container, Button, Link} from '@mui/material';
 import {CopyToClipboard} from 'react-copy-to-clipboard';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const About = () => {
 
@@ -12,9 +13,9 @@ const About = () => {
       <Typography variant="h3" component="h1">About Page</Typography>
       <Typography variant="h5" component="h2">This app was developed by Julian Swerdlin in March 2022.</Typography>
       <Typography variant="h6" component="h3">I used the MERN Stack to develop it. If you think you can make something better, you can clone or fork the Github's Repo and post it!</Typography>
-      <Typography variant="h6" component="h3">If you want to know more about me, you can visit my <a href="https://www.linkedin.com/in/julian-swerdlin/">LinkedIn</a>.</Typography>      
+      <Typography variant="h6" component="h3">If you want to know more about me and my projects, you can visit my <Link href="https://github.com/juliswer" target="_blank"><GitHubIcon />Github</Link></Typography>      
       <CopyToClipboard onCopy={() => setCopy(true)} text="git clone https://github.com/juliswer/mern-crud.git">
-        <Button variant="outlined" color="warning" endIcon={<ContentCopyIcon />}>{copy ? `It's in your clipboard!` : 'Copy'}</Button>
+        <Button variant="outlined" color="warning" endIcon={<ContentCopyIcon />}>{copy ? `It's in your clipboard!` : 'Copy clone link (git)'}</Button>
       </CopyToClipboard>
     </Container>
   )
